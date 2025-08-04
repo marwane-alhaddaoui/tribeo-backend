13cb951ca938cf42a0f31ba055634eecb573f7a2
Marwane Al haddaoui <m.alhaddaoui.000991@stu.brucity.education>
2025-08-05 00:30:29 +0200

    feat(auth): add JWT login and token refresh endpoints

--- core/apps/users/views/auth_views.py
+++ core/apps/users/views/auth_views.py
@@ -1,8 +1,17 @@
 from rest_framework import generics
 from users.models import CustomUser
 from users.serializers.user_serializer import RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = []  # Accessible sans être connecté
+    
+    
+class LoginView(TokenObtainPairView):
+    """
+    POST: /api/auth/login/
+    Body: {"email": "user@test.com", "password": "password123"}
+    """
+    pass
