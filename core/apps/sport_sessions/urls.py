8b4598e510badc686edbb2bea1bb0da21b644135
Marwane Al haddaoui <m.alhaddaoui.000991@stu.brucity.education>
2025-08-05 18:32:44 +0200

    feat(sport_sessions): add detail view for get, update and delete session with permissions check

--- core/apps/sport_sessions/urls.py
+++ core/apps/sport_sessions/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from sport_sessions.views.list_create_view import SessionListCreateView
 from sport_sessions.views.join_session_view import JoinSessionView
 from sport_sessions.views.leave_session_view import LeaveSessionView
+from sport_sessions.views.detail_session_view import SessionDetailView
 
 urlpatterns = [
     path('', SessionListCreateView.as_view(), name='sessions-list-create'),
+    path('<int:pk>/', SessionDetailView.as_view(), name='session-detail'),
     path('<int:pk>/join/', JoinSessionView.as_view(), name='join-session'),
     path('<int:pk>/leave/', LeaveSessionView.as_view(), name='leave-session'),
+    
 ]
---
+++ core/apps/sport_sessions/views/detail_session_view.py
@@ -0,0 +1,26 @@
+from rest_framework import generics, permissions
+from rest_framework.exceptions import PermissionDenied
+from sport_sessions.models import SportSession
+from sport_sessions.serializers.session_serializer import SessionSerializer
+
+class SessionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SportSession.objects.all()
+    serializer_class = SessionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        obj = super().get_object()
+        return obj
+
+    def perform_update(self, serializer):
+        session = self.get_object()
+        # Vérifier que l'utilisateur est le créateur
+        if self.request.user != session.creator:
+            raise PermissionDenied("Vous n'avez pas l'autorisation de modifier cette session.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        # Vérifier que l'utilisateur est le créateur
+        if self.request.user != instance.creator:
+            raise PermissionDenied("Vous n'avez pas l'autorisation de supprimer cette session.")
+        instance.delete()
